b14cd9faa1d42acb8b21eb41c3ef436557ff13c4
liliang9693 <liliang_wk@163.com>
2022-07-12 01:20:41 +0800

    0.0.1

---
+++ python/main.ts
@@ -0,0 +1,23 @@
+//% color="#4571df" iddconWidth=50 iconHeight=40
+namespace bme280{
+
+    //% block="BME280 begin while success" blockType="command" 
+    export function bme280_init(parameter: any, block: any) {
+
+        Generator.addImport("from pinpong.libs.dfrobot_bme680 import DFRobot_BME680");
+        Generator.addCode("bme280 = BME280()");
+   
+    }
+
+
+    //% block="BME280  read [DAT] in data" blockType="reporter" 
+    //% DAT.shadow="dropdown"  DAT.options="DAT"
+    export function bme280_read_data(parameter: any, block: any) { 
+        let dat=parameter.DAT.code;
+        Generator.addCode( [`bme280.${dat}()`,Generator.ORDER_UNARY_POSTFIX]);
+   
+   }
+   
+
+   
+}
